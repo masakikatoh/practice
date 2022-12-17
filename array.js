12651e3f65e92a8eb955753c6323c00b8a1e13c0
masakikatoh <masaki.kato008@gmail.com>
2022-12-17 23:46:09 +0900

    feat(): scraping

--- array.js
+++ array.js
@@ -1,33 +1,56 @@
-// 配列を定義
-let array1 = ["a","b","c","d","e","h"]
-console.log(array1);
-
-// utilities.sleep(staySecond * 1000);
-
-let array2 = ["b","c","d","f","g"]
-console.log(array2);
-
-// 削除された要素を返す 期待する値：a,e,h
-/*
-  indexOf()：指定された値が最初に現れたインデックスを返す。値が見つからない場合は -1 を返す
-  url2DBの1番目から順に、、？
-
-  iは何を示す？
-  -1が返ってくる=取得した配列が前の配列に存在しないため、削除された要素が返ってくる
-*/
-const arraydel = array1.filter(i => array2.indexOf(i) == -1)
-console.log(arraydel);
-
-// アロー関数使わない場合
-const arraydel02 = array1.filter(
-    function(i){
-        return array2.indexOf(i) == -1
-    }
-)
-console.log(arraydel02);
-
-
-// 追加された要素を返す 期待する値：f,g
-  // 
-const arrayadd = 
-console.log(arrayadd);
+function myFunction() {
+    const URL = "https://**";
+    var response = UrlFetchApp.fetch(URL);
+    var content = response.getContentText("UTF-8");
+    var staySecond = 5;
+  
+  // Parserライブラリ発動*from~toでスクリプトの抽出範囲を指定する
+  let url = Parser.data(content).from('mouseout-&gt;product#resetHeader" href="').to('">').iterate();
+  
+  let beforeUrl = url
+  
+  Utilities.sleep(staySecond * 1000);
+  
+  let afterUrl = url
+  
+  // 追加された要素を返す
+  let added = afterUrl.filter(x => !beforeUrl.includes(x));
+  let addedUrls = added.map(added => 'https://**' + added);
+  
+  // 削除された要素を返す
+  let removed = beforeUrl.filter(i => afterUrl.indexOf(i) == -1)
+  let removedUrls = removed.map(removed => 'https://**' + removed);
+  
+  if(added.length!== 0 || removed.length !== 0){
+  //slack通知
+    let postUrl  = "https://**"
+    let userName = " update"   // Slackに通知する時の名前になります
+    let addedMessage  = addedUrls.join("\n") // 送信するメッセージ
+    let removedMessage  = removedUrls.join("\n") // 送信するメッセージ
+  
+  // ここで以下をオプションに設定できます
+  // username(通知者の名前)
+  // icon_emoji(アイコン)
+  // text (送信するメッセージ)
+      let jsonData = {
+        "username" : userName,
+        "text" : 
+            '追加されたデータ\n'+ addedMessage + '\n' + '削除されたデータ\n'+ removedMessage
+      }  
+  
+   // 上の送信内容を設定  
+    let payload = JSON.stringify(jsonData)
+  
+   // オプションを設定
+    let options =
+    {
+      "method" : "post",
+      "contentType" : "application/json",
+      "payload" : payload
+    };
+  
+   // Slackに通知する
+     UrlFetchApp.fetch(postUrl, options);
+  }
+  }
+  
