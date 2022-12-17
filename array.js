6dd0b7b127153c1ffaa475754e0bcf4ae06c71aa
masakikatoh <masaki.kato008@gmail.com>
2022-12-17 12:24:58 +0900

    chore: edited comment

--- array.js
+++ array.js
@@ -1,28 +1,33 @@
-// スクレイピングにより配列を一定間隔開けて生成して、追加、削除があった場合その要素を通知する
-let url2DA = ["a","b","c","d","e","h"]
-console.log(url2DA);
+// 配列を定義
+let array1 = ["a","b","c","d","e","h"]
+console.log(array1);
 
 // utilities.sleep(staySecond * 1000);
 
-let url2DB = ["b","c","d","f","g"]
-console.log(url2DB);
+let array2 = ["b","c","d","f","g"]
+console.log(array2);
 
-// 削除された要素を返す a,e,h
-  // indexOf()：指定された値が最初に現れたインデックスを返す。値が見つからない場合は -1 を返す
-  // 上記は-1が返ってくる=前の配列に存在しない値のため、削除された要素が返ってくる
-const arraydel = url2DA.filter(i => url2DB.indexOf(i) == -1)
+// 削除された要素を返す 期待する値：a,e,h
+/*
+  indexOf()：指定された値が最初に現れたインデックスを返す。値が見つからない場合は -1 を返す
+  url2DBの1番目から順に、、？
+
+  iは何を示す？
+  -1が返ってくる=取得した配列が前の配列に存在しないため、削除された要素が返ってくる
+*/
+const arraydel = array1.filter(i => array2.indexOf(i) == -1)
 console.log(arraydel);
 
 // アロー関数使わない場合
-const arraydel02 = url2DA.filter(
+const arraydel02 = array1.filter(
     function(i){
-        return url2DB.indexOf(i) == -1
+        return array2.indexOf(i) == -1
     }
 )
 console.log(arraydel02);
 
 
-// 追加された要素を返す f,g
+// 追加された要素を返す 期待する値：f,g
   // 
 const arrayadd = 
 console.log(arrayadd);
